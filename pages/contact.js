b4b2e6b1f21a57b9e32c35f354d7a87f057ca9f2
AzmiBhai <azmibhai98@gmail.com>
2022-05-23 22:48:17 +0530

    first commit frontend

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div className='container mx-auto'>contact</div>
+  )
+}
+
+export default contact
---
+++ pages/products.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const products = () => {
+  return (
+    <div className='container mx-auto'>products</div>
+  )
+}
+
+export default products
